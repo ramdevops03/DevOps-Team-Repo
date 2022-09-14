c6a98104fe95a3704b0626225014874ab2e051db
buraashok <46172238+buraashok@users.noreply.github.com>
2022-09-14 19:19:04 -0400

    updating terraform code

---
+++ ec2.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+  region     = "us-east-1"
+  access_key = "__ACCESSKEY__"
+  secret_key = "__SECRETKEY__"
+}
+
+resource "aws_instance" "ec2instance" {
+  ami           = "ami-05fa00d4c63e32376"
+  instance_type = "t2.micro"
+  subnet_id = "subnet-06577657b4ae427f9"
+  key_name = "firstawskey"
+  tags = {
+    Name ="SERVER"
+    Environment = "DEV"
+    OS = "Linux"
+    Managed = "IAC"
+  }
+}
